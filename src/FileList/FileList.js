6f3186a6f934b79f66b89a3b23a36ea0a655e6a9
Correy Lim <correylim@gmail.com>
2021-07-12 09:56:32 -0700

    Render list of PDFs available in FileList

--- src/FileList/FileList.js
+++ src/FileList/FileList.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
+import './FileList.css';
+
 const FileList = () => {
-  return <div>Hello World</div>;
+  const inputDirectory = '/files/';
+  const fileList = [
+    'demo',
+    'PDFTRON_about',
+    'quote',
+  ];
+
+  return (
+    <div id='file-list'>
+      {
+        fileList.map((fileName, idx) => (
+          <div
+            key={`file_${idx}`}
+          >
+            {`${fileName}.pdf`}
+          </div>
+        ))
+      }
+    </div>
+  );
 };
 
 export default FileList;
