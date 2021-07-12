b989f6844f155e4f7bfeab3605a9f9ea233017a2
Correy Lim <correylim@gmail.com>
2021-07-12 13:42:58 -0700

    Add function to return array of pages based on str
    Add a function to parse a string for `,` and `-` characters and output
    an array of page numbers based on the input, ensuring users are able to
    specify either a range of pages, or delimit a certain subset of pages to
    extract

--- src/PageSplit/PageSplit.js
+++ src/PageSplit/PageSplit.js
@@ -25,3 +25,55 @@ const PageSplit = (props) => {
 };
 
 export default PageSplit;
+
+/**
+ * Modified version of the functions implemented here:
+ * https://github.com/PDFTron/webviewer-ui/blob/7.3/src/helpers/getPageArrayFromString.js
+ *
+ * Note that PageLabels implementation has been stripped out, as this PoC app
+ * assumes that all Page Labels correspond to their Page Numbers
+ */
+const getPageArrayFromString = (totalPages, customInput) => {
+  const pagesToPrint = [];
+
+  // User has provided no input, assume every page is desired
+  if (!customInput) {
+    for (let i = 1; i <= totalPages; i++) {
+      pagesToPrint.push(parseInt(i, 10));
+    }
+    return pagesToPrint;
+  }
+
+  const pageGroups = customInput.split(',');
+  pageGroups.forEach(pageGroup => {
+    const range = pageGroup.split('-');
+    const isSinglePage = range.length === 1;
+    const isRangeOfPages = range.length === 2;
+
+    if (isSinglePage) {
+      pagesToPrint.push(parseInt(range[0], 10));
+    } else if (isRangeOfPages) {
+      const start = range[0];
+      let end;
+
+      if (range[1] === '') {
+        // range like 4- means page 4 to the end of the document
+        end = totalPages;
+      } else {
+        end = range[1];
+      }
+
+      for (let i = start; i <= end; i++) {
+        pagesToPrint.push(parseInt(i, 10));
+      }
+    }
+  });
+
+  return pagesToPrint
+    .filter((pageNumber, index, pagesToPrint) => {
+      const isUnique = pagesToPrint.indexOf(pageNumber) === index;
+      const isValidPageNumber = pageNumber > 0 && pageNumber <= totalPages;
+      return isUnique && isValidPageNumber;
+    })
+    .sort((a, b) => a - b);
+};
